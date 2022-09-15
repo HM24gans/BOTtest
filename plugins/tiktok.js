79bbf662cd9c400c36b8c49204730a0337311222
HM24gans <112872541+HM24gans@users.noreply.github.com>
2022-09-15 17:48:17 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -3,13 +3,14 @@ let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
 	if (!args[0]) throw `Link tiktoknya mana?\n\ncontoh:\n${usedPrefix}${command} https://vm.tiktok.com/ZGJAmhSrp/`
     tiktokdlv3(args[0]).then(r => {
     let video = r.video.no_watermark
-    conn.sendFile(m.chat, video, '', `*${wm}*`, m)
+    conn.sendFile(m.chat, video, '', `${command} *Downloader By Hudha*
+*Suport BOT Agar Terus Akfif*
+http://saweria.co/HudhaBot`, m)
     })
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.limit = true
-handler.group = true
 
 handler.command = /^(tt|tiktok|tik)$/i
 
